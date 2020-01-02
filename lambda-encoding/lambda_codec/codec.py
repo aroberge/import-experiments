ffe30c19b5e4c25418d29a4ee5d3844dca5dc084
André Roberge <andre.roberge@gmail.com>
2020-01-02 15:46:23 -0400

    slight reformating; improving readme

--- lambda-encoding/lambda_codec/codec.py
+++ lambda-encoding/lambda_codec/codec.py
@@ -1,7 +1,13 @@
-# See https://docs.python.org/dev/library/codecs.html
+""" Simple demo codec
 
-# encode: str -> bytes
-# decode: bytes -> str
+This codec replaces any Python identifier (token) represented by the
+single Greek letter 'λ' by the corresponding string 'lambda' which is
+the Python keyword.
+
+The source is assumed to be encoded in utf-8.
+To help identify which parts are optional, we use USE_LAMBDA as a global
+variable. Setting it to False, will default to using the normal utf-8 encoding.
+"""
 
 import codecs
 import encodings
@@ -9,10 +15,12 @@ import tokenize
 
 from io import StringIO
 
+utf8 = encodings.search_function("utf8")
 USE_LAMBDA = True
 
 
 def lambda_transform_string(text):
+    """Simple transformation: replaces any single token λ by lambda"""
     tokens = []
     for tok_type, tok_string, _, _, _ in tokenize.generate_tokens(
         StringIO(text).readline
@@ -20,38 +28,33 @@ def lambda_transform_string(text):
         if tok_string == "λ":
             tok_string = "lambda"
         tokens.append((tok_type, tok_string))
-
     return tokenize.untokenize(tokens)
 
 
 def lambda_decode(input, errors="strict"):
-    utf8 = encodings.search_function("utf8")
-    text, _ = utf8.decode(input, errors)
+    text, length = utf8.decode(input, errors)
     if USE_LAMBDA:
         text = lambda_transform_string(text)
-    return text, len(text)
+    return text, length
 
 
 class LambdaIncrementalDecoder(encodings.utf_8.IncrementalDecoder):
-
     def decode(self, input, final=False):
         self.buffer += input
         if final:
             buff = self.buffer
-            self.buffer = b''
+            self.buffer = b""
             if USE_LAMBDA:
-                text, length = lambda_decode(buff)
+                text, _ = lambda_decode(buff)
                 buff = text.encode("utf-8")
             return super().decode(buff, final=True)
         else:
-            return ''
+            return ""
 
 
 def search_function(encoding):
     if encoding != "lambda-encoding":
         return None
-    # Assume utf8 encoding
-    utf8 = encodings.search_function("utf8")
 
     return codecs.CodecInfo(
         name="lambda-encoding",
--- lambda-encoding/readme.md
+++ lambda-encoding/readme.md
@@ -2,9 +2,8 @@
 
 This is an experiment to create a custom encoding where the Greek letter
 λ would be interpreted/converted as the Python lambda keyword.
-The purpose is to learn what is needed to create a custom encoding
-and use it.
-
+The purpose is to learn what is needed to create a custom encoding,
+and making it available for all programs without implicitly importing it.
 
 My original motivation for looking at codecs was from
 [a comment left on one of my blog posts](https://aroberge.blogspot.com/2015/10/from-experimental-import-somethingnew.html?showComment=1444820092247#c5491554166542490140)
@@ -15,4 +14,27 @@ as a starting guide for this much simpler project.
 
 ## How to use
 
-More to come
+Either on the first or second line of your Python script, insert a line like
+
+    # coding: lambda-encoding
+
+I would have prefered to use
+
+    # coding: λ-encoding
+
+but, alas, the name of the encoding has to use only ascii characters.
+
+A sample program, `test.py`, is included for convenience.
+You can then run that program or your own following one of the methods described below.
+
+### Environment variable
+
+We assume you are in the same directory where this file and the
+file `usercustomize.py` are located.  You can set the environment variable
+PYTHONPATH to be equal to this directory. On Windows, this can be done by
+
+    set PYTHONPATH=%CD%
+
+Doing so will ensure that Python will executes `usercustomize.py` prior
+to executing any user code. The file `usercustomize.py` does the
+require import to ensure that the lambda-encoding codec is registered.
